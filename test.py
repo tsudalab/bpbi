29c39ed63d4728297fe74a1e8157051e3507fbec
Terayama <terayama@cbms.k.u-tokyo.ac.jp>
2017-09-10 16:42:43 +0900

    test script

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import pandas
 import bai_algorithms
 
 def open_with_pandas(filename):
-    df = pandas.read_csv(filename)
+    df = pandas.read_csv('data/'+filename)
     data = df.values
     rmsd = data[:,1]
     data = data[:,2:22]
@@ -37,8 +37,8 @@ def check_pose(estimated_pose, rmsd):
 
 
 if __name__ == "__main__":
-    #Select ligand ID. (2R3J-SCJ, 1OI9-N20, 1KE6-LS2, 3DDQ-RRC)
-    ligand_id = '2R3J-SCJ'
+    #Select ligand ID. (2R3J-SCJ, 1OI9-N20, 1KE6-LS2, 3DDQ-RRC, 2WYG-461, 2J94-G15, 2VCI-2GJ, 3VHD-VHE)
+    ligand_id = '2J94-G15'
     
     
     #Load RMSD and 20 binding free energeis estimated by MM-PBSA of each binding pose.
@@ -49,7 +49,9 @@ if __name__ == "__main__":
     
     #----Explore a binding pose using Uniform Sampling
     #Paramter: number of budget
-    best_pose, means, nums = bai_algorithms.uniform_sampling(data, budget = 40)
+    budget = 40
+    best_pose, means, nums = bai_algorithms.uniform_sampling(data, budget = budget)
+    print '#### Uniform Sampling ####'
     print 'best pose ID:', best_pose, ', estimated means:',  means, ', pulled arms:', nums    
     #Check the estimated pose is correct (<2.0 Å) or not.
     print check_pose(best_pose, rmsd)
@@ -57,9 +59,12 @@ if __name__ == "__main__":
     
     
     #----Explore a binding pose using UCB(p) sampling
-    #Paramter: number of budget
-    best_pose, means, nums = bai_algorithms.UCBp_sampling(data, budget = 30, p = 8)
+    #Paramter: number of budget, exploration parameter p
+    budget = 30
+    p = 8
+    best_pose, means, nums = bai_algorithms.UCBp_sampling(data, budget = budget, p = p)
     #Check the result    
+    print '#### UCB(p) p =', str(p), ' ####'
     print 'best pose ID:', best_pose, ', estimated means:',  means, ', pulled arms:', nums    
     #Check the estimated pose is correct (<2.0 Å) or not.
     print check_pose(best_pose, rmsd)
@@ -67,9 +72,12 @@ if __name__ == "__main__":
 
     
     #----Explore a binding pose using UCB-E sampling
-    #Paramter: number of budget
-    best_pose, means, nums = bai_algorithms.UCBE_sampling(data, budget = 30, C = 32)
+    #Paramter: number of budget, exploration parameter c
+    budget = 30
+    c = 32
+    best_pose, means, nums = bai_algorithms.UCBE_sampling(data, budget = budget, C = c)
     #Check the estimated pose is correct (<2.0 Å) or not.
+    print '#### UCB-E c =', str(c), ' ####'
     print 'best pose ID:', best_pose, ', estimated means:',  means, ', pulled arms:', nums    
     #Check the estimated pose is correct (<2.0 Å) or not.
     print check_pose(best_pose, rmsd)
@@ -77,22 +85,46 @@ if __name__ == "__main__":
     
     #----Explore a binding pose using Successive Rejection
     #Paramter: number of budget
-    best_pose, means, nums = bai_algorithms.SR(data, budget = 30)
-    #Check the estimated pose is correct (<2.0 Å) or not.
+    budget = 30
+    best_pose, means, nums = bai_algorithms.SR(data, budget = budget)
+    #Check the estimated pose is correct (<2.0 Å) or not
+    print '#### SR ####'
     print 'best pose ID:', best_pose, ', estimated means:',  means, ', pulled arms:', nums    
     #Check the estimated pose is correct (<2.0 Å) or not.
     print check_pose(best_pose, rmsd)
     
      
     #----Explore a binding pose using UGapE sampling
-    #Paramter: number of budget
-    best_pose, means, nums = bai_algorithms.UGapE(data, budget = 30, a = 0.25)
+    #Paramter: number of budget, exploration parameter a
+    budget = 30
+    a = 0.25
+    best_pose, means, nums = bai_algorithms.UGapE(data, budget = budget, a = a)
     #Check the estimated pose is correct (<2.0 Å) or not.
+    print '#### UGapE a =', str(a), ' ####'
     print 'best pose ID:', best_pose, ', estimated means:',  means, ', pulled arms:', nums    
     #Check the estimated pose is correct (<2.0 Å) or not.
     print check_pose(best_pose, rmsd)
+
+
+    #----Explore a binding pose using UCB-E auto                                                                                                               
+    #Paramter: number of budget                                                                                                          
+    budget = 40
+    best_pose, means, nums = bai_algorithms.UCBE_adaptive(data, budget = budget)
+    #Check the estimated pose is correct (<2.0 Å) or not.                                                                                                         
+    print '#### UCB-E adaptive ####'
+    print 'best pose ID:', best_pose, ', estimated means:',  means, ', pulled arms:', nums
+    #Check the estimated pose is correct (<2.0 Å) or not.                                                                                                         
+    print check_pose(best_pose, rmsd)
     
-    
+    #----Explore a binding pose using UGapE auto                                                                                                             
+    #Paramter: number of budget                                                                                                   
+    budget = 40
+    best_pose, means, nums = bai_algorithms.UGapE_adaptive(data, budget = budget)
+    #Check the estimated pose is correct (<2.0 Å) or not.                                                                                                 
+    print '#### UGapE a =', str(a), ' ####'
+    print 'best pose ID:', best_pose, ', estimated means:',  means, ', pulled arms:', nums
+    #Check the estimated pose is correct (<2.0 Å) or not.                                                                                             
+    print check_pose(best_pose, rmsd)    
     
     
     #----Calculation example of prob. of correct pose prediction using uniform sampling 
@@ -100,7 +132,7 @@ if __name__ == "__main__":
     true_pose_index = filter(lambda x:rmsd[x]<=2.0 , range(20))
     print 'hit_ratio', calc_hit_ratio(result, true_pose_index)   
     
-    
+
     
     
     
